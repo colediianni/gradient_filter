b002a5b8006c1cb7e74bb04308f92d8d84417c90
colediianni <cwdiianni@gmail.com>
2022-10-10 15:48:45 -0500

    renamed file

--- classification_training_pipeline.py
+++ classification_training_pipeline.py
@@ -1,7 +1,7 @@
 from data import load_data
 from model import get_classification_model, train_classification_model
 
-def train_classification_model(model_type, dataset_name, device, model_save_path, epochs=100, lr=0.001, model_load_path=""):
+def classification_training_pipeline(model_type, dataset_name, device, model_save_path, epochs=100, lr=0.001, model_load_path=""):
     # load datasets
     train_loader, val_loader, test_loader = load_data(dataset=dataset_name, batch_size=16, train_prop=0.8, training_gan=False)
 
@@ -10,4 +10,3 @@ def train_classification_model(model_type, dataset_name, device, model_save_path
 
     # train model
     network = train_classification_model(network, model_save_path, epochs=epochs, lr=lr)
-    
