7c0b22825e7c30bed5c8f871c8862e205efa4404
colediianni <cwdiianni@gmail.com>
2022-10-11 20:41:11 -0500

    moved model testing to model.py

--- test_classification_model.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-def test_model(base_path, model_type, dataset_name, device, model_load_path):
-
-    output_file = base_path+"/logs/"+model_type+"_"+dataset_name+".txt"
-
-    test(network, test_loader, device, output_file)
