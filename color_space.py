7488cd4ca29124c44afee3a985754f65b8152cb9
colediianni <cwdiianni@gmail.com>
2022-10-15 22:52:15 -0500

    Update color_space.py

--- color_space.py
+++ color_space.py
@@ -1,14 +1,15 @@
 from torchvision import transforms
 import cv2
+from numpy import asarray
 
 colorspaces = {
-    "lab":[transforms.Lambda(lambda img: img.detach().to("cpu").numpy()),
+    "lab":[transforms.Lambda(lambda img: asarray(img)),
         transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2Lab)),
         transforms.ToTensor()],
-    "xyz":[transforms.Lambda(lambda img: img.detach().to("cpu").numpy()),
+    "xyz":[transforms.Lambda(lambda img: asarray(img)),
         transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2XYZ)),
         transforms.ToTensor()],
-    "hsv":[transforms.Lambda(lambda img: img.detach().to("cpu").numpy()),
+    "hsv":[transforms.Lambda(lambda img: asarray(img)),
         transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2HSV)),
         transforms.ToTensor()],
     "rgb":[transforms.ToTensor()]
