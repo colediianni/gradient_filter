4845c17f5b6ee31a68397662c83bbe4fb3a3192c
cbbcbail <cbbcbail@gmail.com>
2022-10-15 22:36:51 -0500

    colorspace transformers simplified

--- color_space.py
+++ color_space.py
@@ -2,17 +2,11 @@ from torchvision import transforms
 import cv2
 
 colorspaces = {
-    "lab":[transforms.Compose([
-        transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2Lab)),
-        transforms.ToTensor(),
-    ])],
-    "xyz":[transforms.Compose([
-        transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2XYZ)),
-        transforms.ToTensor(),
-    ])],
-    "hsv":[transforms.Compose([
-        transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2HSV)),
-        transforms.ToTensor(),
-    ])],
+    "lab":[transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2Lab)),
+        transforms.ToTensor()],
+    "xyz":[transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2XYZ)),
+        transforms.ToTensor()],
+    "hsv":[transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2HSV)),
+        transforms.ToTensor()],
     "rgb":[transforms.ToTensor()]
 }
