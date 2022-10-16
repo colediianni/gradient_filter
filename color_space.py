d2caac3864ee2f63f2d63c5ae816d566737fb1b9
cbbcbail <cbbcbail@gmail.com>
2022-10-15 22:29:32 -0500

    colorspace transformers

--- color_space.py
+++ color_space.py
@@ -1,3 +1,18 @@
 from torchvision import transforms
+import cv2
 
-colorspaces = {"rgb":[transforms.ToTensor()]}
+colorspaces = {
+    "lab":[transforms.Compose([
+        transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2Lab)),
+        transforms.ToTensor(),
+    ])],
+    "xyz":[transforms.Compose([
+        transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2XYZ)),
+        transforms.ToTensor(),
+    ])],
+    "hsv":[transforms.Compose([
+        transforms.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2HSV)),
+        transforms.ToTensor(),
+    ])],
+    "rgb":[transforms.ToTensor()]
+}
