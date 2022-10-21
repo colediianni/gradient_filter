1a1ccd13e42649f84a922ac993b2b343a929256b
colediianni <cwdiianni@gmail.com>
2022-10-20 19:26:52 -0500

    Update gan.py

--- gan.py
+++ gan.py
@@ -69,7 +69,7 @@ class Discriminator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
-            MyColorInvariantConv2d(nc, ndf, 4, 2, 1, bias=False),
+            EuclideanColorInvariantConv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
