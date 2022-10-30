2cce1a2c15fa56e62af264b765541cdb185dd4e3
colediianni <cwdiianni@gmail.com>
2022-10-30 16:59:04 -0500

    Update augmentations.py

--- augmentations.py
+++ augmentations.py
@@ -97,14 +97,14 @@ class remove_color(torch.nn.Module):
         padded_input = self.padding(image)
         unfold = torch.nn.Unfold(kernel_size=(image.shape[2], image.shape[3]), padding=0, stride=1)
         inp_unf = unfold(padded_input)
-        # print("inp_unf", inp_unf.shape)
+        print("inp_unf3", inp_unf.shape)
         inp_unf = inp_unf.transpose(1, 2)
         inp_unf = inp_unf.reshape((image.shape[0], -1, 3, image.shape[2], image.shape[3]))
         inp_unf = inp_unf.permute([0, 1, 3, 4, 2])
         gradient_image = torch.zeros(image.shape[0], inp_unf.shape[1], inp_unf.shape[2], inp_unf.shape[3]).to(self.device)
         image = image.permute([0, 2, 3, 1])
         # print(gradient_image.shape)
-        # print("inp_unf", inp_unf.shape)
+        print("inp_unf4", inp_unf.shape)
         # print(image.shape)
 
         for compare_shift in range(inp_unf.shape[1]):
