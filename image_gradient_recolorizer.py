4066bae7789999a2b7a20275beca4e86f1451ee6
colediianni <cwdiianni@gmail.com>
2022-11-12 02:41:47 -0600

    Update image_gradient_recolorizer.py

--- image_gradient_recolorizer.py
+++ image_gradient_recolorizer.py
@@ -76,7 +76,7 @@ def colorize_gradient_image(original_image, device, bias_color_location=[], weig
     updated_colorized_images = padding(updated_colorized_images).requires_grad_(requires_grad=True)
     updated_colorized_images.retain_grad()
     print(updated_colorized_images)
-    updated_colorized_images.register_hook(print)
+    print(updated_colorized_images.is_leaf)
 
     # plt.imshow(remove_infs(colorized_images[0].permute([1, 2, 0])).cpu().detach().numpy())
     # plt.show()
@@ -97,6 +97,7 @@ def colorize_gradient_image(original_image, device, bias_color_location=[], weig
         weight = 1
 
       predicted_gradients = torch.abs(updated_colorized_images[:, :, neighbor_y_shift:neighbor_y_shift+h, neighbor_x_shift:neighbor_x_shift+w] - updated_colorized_images[:, :, receptive_field:receptive_field+h, receptive_field:receptive_field+w]).permute([0, 2, 3, 1]).sum(dim=-1)
+      print(predicted_gradients.is_leaf)
       # print(predicted_gradients)
       # print("predicted_gradients", predicted_gradients.max())
       # print("gradient_image", gradient_image.max())
