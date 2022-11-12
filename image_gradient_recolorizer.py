902cad10f43f5762998d0910f3b46c488b2ef19c
colediianni <cwdiianni@gmail.com>
2022-11-12 02:51:02 -0600

    Update image_gradient_recolorizer.py

--- image_gradient_recolorizer.py
+++ image_gradient_recolorizer.py
@@ -96,7 +96,7 @@ def colorize_gradient_image(original_image, device, bias_color_location=[], weig
         weight = 1
 
       predicted_gradients = torch.abs(updated_colorized_images[:, :, neighbor_y_shift:neighbor_y_shift+h, neighbor_x_shift:neighbor_x_shift+w] - updated_colorized_images[:, :, receptive_field:receptive_field+h, receptive_field:receptive_field+w]).permute([0, 2, 3, 1]).sum(dim=-1)
-      # print(predicted_gradients)
+      print(predicted_gradients)
       # print("predicted_gradients", predicted_gradients.max())
       # print("gradient_image", gradient_image.max())
 
@@ -104,7 +104,6 @@ def colorize_gradient_image(original_image, device, bias_color_location=[], weig
           diff_to_diff += (1/weight) * torch.mul(torch.abs(predicted_gradients - gradient_image[:, direction]), usable_gradients[:, direction]).sum()
       elif squared_diff:
           diff_to_diff += (1/weight) * torch.mul(torch.square(predicted_gradients - gradient_image[:, direction]), usable_gradients[:, direction]).sum()
-      print(diff_to_diff.is_leaf)
 
     # print("diff_to_diff", diff_to_diff)
     # backpropogate
