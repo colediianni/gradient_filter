f4a41ab8b199a8046f8ab4073b8cae559fe380bc
colediianni <cwdiianni@gmail.com>
2022-11-20 01:03:31 -0600

    Update image_gradient_recolorizer.py

--- image_gradient_recolorizer.py
+++ image_gradient_recolorizer.py
@@ -162,5 +162,5 @@ def colorize_gradient_image(original_image, device, bias_color_location=[], weig
   # plt.imshow(remove_infs(colorized_images[0].permute([1, 2, 0])).cpu().detach().numpy())
   # plt.show()
 
-  colorized_images /= 255
-  return colorized_images.type(torch.float).to(device)
+  colorized_images = colorized_images.type(torch.float).to(device)
+  return colorized_images / 255
