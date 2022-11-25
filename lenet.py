800de27eda68b2f710f3ad12fd5e86cdd0bb2cc9
colediianni <cwdiianni@gmail.com>
2022-11-25 01:18:31 -0600

    adding resnet file

--- lenet.py
+++ lenet.py
@@ -45,6 +45,7 @@ class LeNet(nn.Module):
         self.fc1 = nn.Linear(120, 84)
         self.relu1 = nn.ReLU()
         self.fc2 = nn.Linear(84, num_classes)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         out = self.layer1(x)
@@ -55,4 +56,5 @@ class LeNet(nn.Module):
         out = self.fc1(out)
         out = self.relu1(out)
         out = self.fc2(out)
+        out = self.softmax(out)
         return out
