217e44759b0c06d26cd960f7ae8548a51570618b
colediianni <cwdiianni@gmail.com>
2022-11-29 13:15:45 -0600

    Update image_gradient_recolorizer.py

--- image_gradient_recolorizer.py
+++ image_gradient_recolorizer.py
@@ -108,7 +108,7 @@ def colorize_gradient_image(original_image, device, bias_color_location=[], weig
     colorized_images = (torch.rand(image_shape)*255).type(torch.int).to(device)
     # print(colorized_images.shape) # torch.Size([128, 3, 64, 64])
     for index in range(len(bias_color_location[0])):
-        colorized_images[:, :, tuple(bias_color_location[1][index])] = torch.tensor(bias_color_location[0]).to(device)
+        colorized_images[:, :, tuple(bias_color_location[1][index])] = torch.tensor(bias_color_location[0][index]).to(device)
 
   # remove same-pixel comparison from gradients
   num_directions = gradient_image.shape[1]
@@ -176,7 +176,7 @@ def colorize_gradient_image(original_image, device, bias_color_location=[], weig
     colorized_images = new_image.detach()
     if using_bias:
         for index in range(len(bias_color_location[0])):
-            colorized_images[:, :, tuple(bias_color_location[1][index])] = torch.tensor(bias_color_location[0]).to(device)
+            colorized_images[:, :, tuple(bias_color_location[1][index])] = torch.tensor(bias_color_location[0][index]).to(device)
 
   if verbose:
       plt.imshow(remove_infs(colorized_images[0].permute([1, 2, 0])).cpu().detach().numpy())
